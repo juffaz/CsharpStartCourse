8c6ceecd1a970fab40bb88fb140a53c49d6b1d2e
Sergey K <i@ksergey.ru>
2021-07-27 19:14:00 +0300

    Add project files.

---
+++ CsharpStartCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpStartCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
